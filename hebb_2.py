cc9b6d56d02630d64027c16ca0ff37c9cd136c08
Zeyuan Luo <160167402+LuoZzzzzz@users.noreply.github.com>
2025-04-17 23:24:36 +0100

    added new connection methods

--- hebb_2.py
+++ hebb_2.py
@@ -17,6 +17,9 @@ class Neuron:
         self.is_input = is_input  # Flag for input neurons
         self.hebb = hebb # Hebbian learning rate
 
+        # Neuron index for identification, order is input_neurons, then normal neurons
+        self.index = None
+
     def connect(self, neuron, weight=1.0):
         self.connections.append((neuron, weight))
 
@@ -56,20 +59,54 @@ class NeuralNetwork:
                                            np.zeros(input_size))).T
         self.decay_factor = decay_factor
 
+        # Assign indices to neurons
+        for idx, neuron in enumerate(self.input_neurons + self.neurons):
+            neuron.index = idx
+
+        self.weight_history = {}
+        self.firing_history = {}
+
+        self.input_pattern = {}
+        self.input_pattern_len = None
+
+    # Connect all neurons randomly
     def connect_randomly(self, connection_probability=0.3, weight_range=(0.5, 1.5)):
-        for neuron in self.neurons + self.input_neurons:
+        for neuron in self.input_neurons + self.neurons:
             for other_neuron in self.neurons:
                 if neuron != other_neuron and random.random() < connection_probability:
                     weight = random.uniform(*weight_range)
                     neuron.connect(other_neuron, weight)
+    
+    # Encourage neurons to connect only to those with nearby indices
+    def connect_by_location(self, connection_probability=0.8, weight_range=(0.5, 1.5)):
+        for neuron in self.input_neurons + self.neurons:
+            for other_neuron in self.neurons:
+                if neuron != other_neuron and random.random() < connection_probability*max(0.01, 1/(neuron.index - other_neuron.index)):
+                    weight = random.uniform(*weight_range)
+                    neuron.connect(other_neuron, weight)
+
+    # Encourage neurons to connect only to those with nearby indices
+    def connect_by_group(self, connection_probability=0.8, weight_range=(0.5, 1.5)):
+        for neuron in self.input_neurons + self.neurons:
+            for other_neuron in self.neurons:
+                if (neuron != other_neuron) and \
+                   (random.random() < connection_probability*(1/abs(neuron.index - other_neuron.index))) and \
+                   ((neuron.index < 50 and other_neuron.index < 50) or (neuron.index >= 50 and other_neuron.index >= 50)):
+                    weight = random.uniform(*weight_range)
+                    neuron.connect(other_neuron, weight)
+
+    def set_input_pattern(self, input_pattern): # input_pattern: np array with rows representing pattern of each timestep
+        self.input_pattern_len = len(input_pattern)
+        for step in range(self.input_pattern_len):
+            self.input_pattern[step] = input_pattern[step, :]
 
     def stimulate_inputs(self, step):
-        for input_neuron in self.input_neurons:
-            if (step % 10 == 0) and (step <= 300):  # Periodic firing based on index
-                input_neuron.stimulate(input_potential=1.0)
+        if step < self.input_pattern_len:
+            for input_neuron in self.input_neurons:
+                input_neuron.stimulate(input_potential=self.input_pattern[step][input_neuron.index])
 
     def weight_decay(self):
-        for neuron in self.neurons + self.input_neurons:    # Iterate through all neurons   
+        for neuron in self.neurons:    # Iterate through all neurons (excluding input neurons)  
             for idx in range(len(neuron.connections)):      # Iterate through neuron connections and decay weights
                 neuron.connections[idx] = (neuron.connections[idx][0], self.decay_factor*neuron.connections[idx][1])
 
@@ -79,9 +116,16 @@ class NeuralNetwork:
 
         self.stimulate_inputs(step)
 
+        # Firing status based on new neuron indices
+        firing_status = [0, ] * len(self.input_neurons+self.neurons)
+
         # Record fired neurons
         for neuron in self.input_neurons + self.neurons:
             if neuron.fire():
+                # Update firing status list
+                firing_status[neuron.index] = 1
+
+                # Update old lists
                 if neuron.is_input:
                     fired_input_neurons.append(neuron)
                 else:
@@ -94,6 +138,12 @@ class NeuralNetwork:
         # Decay all weights
         self.weight_decay()
 
+        # Update weight_history dictionary, neuron indices are based on list positions
+        self.weight_history[step] = [[(neuron_.index, weight, neuron.index) for (neuron_, weight) in neuron.connections] for neuron in self.input_neurons+self.neurons]
+
+        # Update firing_history
+        self.firing_history[step] = firing_status
+
         # Return indices of fired input neurons and fired neurons
         return np.asarray([self.input_neurons.index(neuron) for neuron in fired_input_neurons]), np.asarray([self.neurons.index(neuron) for neuron in fired_neurons])
     
@@ -105,3 +155,20 @@ class NeuralNetwork:
             input_neuron_history.append(input_neuron_indices)
             neuron_history.append(neuron_indices)
         return input_neuron_history, neuron_history
+    
+    def set_learning_rate(self, learning_rate):
+        # Only implement hebbian learning for non-input neurons
+        for neuron in self.neurons:
+            neuron.hebb = learning_rate
+
+    def clear_potential(self):
+        for neuron in self.input_neurons + self.neurons:
+            neuron.current_potential = 0
+            neuron.next_potential = 0
+
+    def set_random_potential(self, min_potential=0.0, max_potential=1.0):
+        for neuron in self.input_neurons + self.neurons:
+            neuron.current_potential = 0.0
+            neuron.next_potential = np.random.rand()*(max_potential-min_potential) + min_potential
+
+    
